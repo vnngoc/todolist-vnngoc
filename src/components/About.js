e700bb2bf72242fa422627ef4ebb185d7862cc52
vnngoc <107876602+vnngoc@users.noreply.github.com>
2022-10-29 00:49:43 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,6 +17,8 @@ function About() {
       <div className="container-desc">
         <p className="context">Họ và tên: Vương Minh Ngọc</p>
         <p className="context">MSSV: 46.01.104.124</p>
+        <p className="context">Email: ngocvuong443@gmail.com</p>
+        <p className="context">Khoa: Công nghệ thông tin</p>
         <p className="context">Trường: Đại học Sư phạm TP.HCM</p>
         <p className="context">Lớp: 2211COMP1031</p>
       </div>
